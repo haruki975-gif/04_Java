71ae211690a3c43810109310ff1ab569a5a32843
haruki975-gif <haruki975@gmail.com>
2025-01-20 11:25:24 +0900

    java workspace 등록

---
+++ JavaTest.java
@@ -0,0 +1,15 @@
+public class JavaTest{
+
+  public static void main(String[] args){
+
+    System.out.println("Hello World!!!");
+    System.out.println("어두운 바다의 등불이 되어");
+    System.out.println("나는 선의의 순환을 원한다.");
+
+    for(int i=1; i<5; i++){
+	System.out.println(i+"번 째 회귀?");
+    }
+
+  }
+
+}
