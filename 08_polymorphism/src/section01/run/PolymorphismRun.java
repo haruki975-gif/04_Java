0b6b57d123b2dbdf614d114246061a81a8ead508
haruki975-gif <haruki975@gmail.com>
2025-02-04 17:19:32 +0900

    20250204_다형성

---
+++ 08_polymorphism/src/section01/run/PolymorphismRun.java
@@ -0,0 +1,17 @@
+package section01.run;
+
+import section01.service.PolymorphismService;
+
+public class PolymorphismRun {
+	public static void main(String[] args) {
+		
+		PolymorphismService service = new PolymorphismService();
+		// service.test1();
+		// service.test2(); // Child, Parent, Object 순으로 출력
+		// service.test3();
+		service.test4();
+		
+		// 실행   : ctrl + F11
+		// 디버그 : F11
+	}
+}
