9c69de8db545df1bafde7894b1d030a29c7a6acf
haruki975-gif <haruki975@gmail.com>
2025-02-06 15:21:44 +0900

    20250206_컬렉션

---
+++ 10_generics_collection/src/generics/Box.java
@@ -0,0 +1,37 @@
+package generics;
+
+// Box<T> -> <> : 다이아몬드 연산자
+public class Box<T> {
+	/* 제네릭스(Generics)
+	 * - 뜻 : 일반적인, 속(속해있다)
+	 * - 자바에서 뜻 : 데이터 타입을 일반화한다
+	 *   -> 지정된 타입 하나로 변경(맞춤)
+	 * 
+	 * - 사용 이유
+	 * 1) 타입 안정성 보장(컴파일 단계에서 강력한 타입 검증)
+	 * 2) 타입 체크, 형변환 코드 생략 가능(코드 길이 감소)
+	 * 3) 코드 재사용성 증가
+	 * 
+	 * 
+	 * - 주요 파라미터(매개 변수) 타입 관례
+	 * T : Type (아무 타입이나 작성)
+	 * E : Element (아무 객체 == Object)
+	 * K : Key
+	 * V : Value
+	 * N : Number
+	 * -> 이거 아니어도 마음대로 써도 됨
+	 */
+	
+	private T value; // 객체 생성 시점에 작성된 자료형으로 지정
+	
+	// setter
+	public void setValue(T value) {
+		this.value = value;
+	}
+	
+	// getter
+	public T getValue() {
+		return value;
+	}
+	
+}
---
+++ 10_generics_collection/src/list/service/ListService.java
@@ -0,0 +1,21 @@
+package list.service;
+
+public class ListService {
+	
+	/* [Collection Framework]
+	 * - Java에서 제공하는 자료구조 모음(=뼈대)
+	 * - Java.util 패키지에 존재
+	 * 
+	 * [Collection Framework 특징]
+	 * 1. 크기 제한 X
+	 * 2. 추가, 수정, 삭제, 정렬 등의 기능 제공
+	 * 3. 객체만 저장 가능!!
+	 * 		- 기본 자료형은 Wrapper Class를 이용해서 저장
+	 *    - 기본적으로 타입 제한은 없지만 개발의 편의성을 위해 한 가지 타입으로 제한
+	 *    	-> 제네릭스 이용
+	 */
+	
+	/* [List]
+	 * 
+	 */
+}
