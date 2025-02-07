3ce5994d26f15bb324b459169170cdc5d92b6e0d
haruki975-gif <haruki975@gmail.com>
2025-02-07 17:02:02 +0900

    20250207_List

--- 10_generics_collection/src/list/service/BookService.java
+++ 10_generics_collection/src/list/service/BookService.java
@@ -76,8 +76,118 @@ public class BookService {
 	}
 	
 	
+	/** 전달 받은 index 번째 요소의 가격을 newPrice로 수정
+	 * @param index
+	 * @param newPrice
+	 * @return "[책제목] 가격이 [이전] -> [수정가격]으로 수정되었습니다." 출력
+	 */
+	public String modifyBookPrice2(int index, int newPrice) {
+		BookDTO target = bookList.get(index); // index 번째 요소 얻어오기
+		int oldPrice = target.getPrice(); // 이전 가격 저장
+		target.setPrice(newPrice); // 새 가격으로 수정
+		
+		return String.format("[%s] 가격이 [%d] -> [%d]으로 수정되었습니다.", target.getTitle(), oldPrice, newPrice);
+	}
 	
 	
+	/** index 번째 BookDTO 제거
+	 *  단, 없으면 NULL 반환
+	 *  제거 성공하면 제거된 BookDTO 반환
+	 * @param index
+	 */
+	public BookDTO removeBook(int index) {
+		// index가 존재하는지 검사
+		if(checkIndex(index) == false) return null;
+		
+	  return bookList.remove(index);
+	  // bookList에서 index 번째 요소를 제거
+	  // == index 번째 요소를 뽑아냄 (빈칸을 알아서 채움)
+	}
+	
+	
+	/** 제목이 일치하는 책 반환
+	 * @param title : 찾으려는 책 제목
+	 */
+	public BookDTO selectTitle(String title) {
+		for(BookDTO book : bookList) { // bookList에서 하나씩 꺼냄
+			// 꺼낸 책의 제목과 전달 받은 제목이 같을 경우
+			if(book.getTitle().equals(title)) {
+				return book;
+			}
+		}
+		return null; // 책을 못찾은 경우
+	}
+	
+	
+	/** 제목이 일치하는 책 제거
+	 * @param title : 제거하려는 책 제목
+	 */
+	public boolean removeBookTitle(String title) {
+		for(BookDTO book : bookList) { // bookList에서 하나씩 꺼냄
+			
+			// 꺼낸 책의 제목과 전달 받은 제목이 같을 경우
+			if(book.getTitle().equals(title)) {
+				
+				return bookList.remove(book); // 삭제 성공 시 true
+				// -> bookList에 저장된 객체(BookDTO)가
+				//    equals() 오버라이딩이 되어있어야 사용 가능한 방법
+			}
+		}
+		return false; // 삭제 성공 시 false
+	}
+	
+	
+	/** 입력 받은 publisher와 같은 출판사를 가지는 책을 모두 조회
+	 * @param pub : 출판사명
+	 */
+	public List<BookDTO> selectPublisherAll(String publisher) {
+		
+		// 출판사명이 일치하는 책만 저장해둘 List 생성
+		List<BookDTO> list = new ArrayList<BookDTO>();
+		
+		for(BookDTO book : bookList) { // bookList에서 하나씩 꺼냄
+      if(book.getPublisher().equals(publisher)) { // 출판사가 일치하면
+      	list.add(book); // 리스트에 추가
+      }
+		}
+		return list; // List에 저장된 요소의 개수 : 0 ~ n개
+	}
+	
+	
+	/** 입력 받은 author와 같은 저자를 가지는 책을 모두 조회
+	 * @param author : 저자
+	 * @return : 0 ~ n개의 결과를 가지는 List
+	 */
+	public List<BookDTO> selectAuthorAll(String author) {
+		
+		List<BookDTO> list = new ArrayList<BookDTO>();
+		
+		for(BookDTO book : bookList) {
+			if(book.getAuthor().equals(author)) { // 저자 일치하면
+				list.add(book); // 리스트에 추가
+			}
+		}
+		return list; // List에 저장된 요소의 개수 : 0 ~ n개
+	}
+	
+	
+	/** 검색어가 제목,저자에 포함된 모든 책 조회하기
+	 * @param search : 검색어
+	 * @return : 0 ~ n개의 결과를 가지는 List
+	 */
+	public List<BookDTO> searchBook(String search) {
+		
+		List<BookDTO> list = new ArrayList<BookDTO>();
+		
+		for(BookDTO book : bookList) {
+			// book의 제목이나 저자에 search(키워드)가 포함된 경우
+			if(book.getTitle().contains(search) || book.getAuthor().contains(search)) { // 제목,저자 일치하면
+				list.add(book); // 리스트에 추가
+			}
+		}
+		
+		return list;
+	}
 	
 	
 	
--- 10_generics_collection/src/list/view/BookView.java
+++ 10_generics_collection/src/list/view/BookView.java
@@ -27,7 +27,16 @@ public class BookView {
 			System.out.println("2. index 번호로 조회");
 			System.out.println("3. 책 추가하기");
 			System.out.println("4. 책 가격 수정하기");
-			System.out.println("5. 책 제거하기");
+			System.out.println("5. 책 제거하기(index)");
+			
+			// 추가 메뉴
+			System.out.println("6. 제목이 일치하는 책 1권 조회");
+			System.out.println("7. 제목이 일치하는 책 제거하기");
+			
+			System.out.println("8. 출판사가 일치하는 책 모두 조회하기");
+			System.out.println("9. 저자가 일치하는 책 모두 조회하기");
+			System.out.println("10. 검색어가 제목,저자에 포함된 모든 책 조회하기");
+			
 			System.out.println("0. 종료");
 			System.out.println(); // 줄바꿈
 			
@@ -40,8 +49,17 @@ public class BookView {
 			case 1: selectAll(); break;
 			case 2: selectIndex(); break;
 			case 3: addBook(); break;
-			case 4: modifyBookPrice(); break;
-			case 5:  break;
+			case 4: modifyBookPrice2(); break;
+			case 5: removeBook(); break;
+			
+			case 6: selectTitle(); break;
+			case 7: removeBookTitle(); break;
+			
+			// 서비스 호출 시 반환된 결과가 List<BookDTO>
+			case 8: selectPublisherAll(); break;
+			case 9: selectAuthorAll(); break;
+			case 10: searchBook(); break;
+			
 			case 0: System.out.println("*** 프로그램이 종료됩니다. ***"); break;
 			default : System.out.println("@@@ 메뉴 번호 잘못 입력 @@@");
 			}
@@ -176,20 +194,161 @@ public class BookView {
 		System.out.print("수정할 인덱스 번호 : ");
 		int index = sc.nextInt();
 		
+		// index 범위 초과 시
+		if(service.checkIndex(index) == false) {
+			System.out.println("해당 인덱스에 책이 존재하지 않습니다.");
+		}
+		
+		// 책이 있다면 수정할 가격 입력 받기
+		System.out.print("수정할 가격 입력 : ");
+		int newPrice = sc.nextInt(); // 새로운 가격
+		
+		// 서비스 호출
+		System.out.println(service.modifyBookPrice2(index, newPrice));
 	}
 	
 	
+	/** index 번호를 입력 받아 책 제거
+	 *  단, 해당 index에 책이 없으면 "해당 인덱스에 책이 존재하지 않습니다." 출력
+	 *  있으면 "[책 제목] 책이 제거되었습니다." 출력
+	 */
+	public void removeBook() {
+		System.out.println("\n ### 책 제거하기(index) ### \n");
+		
+		// 제거할 인덱스 번호 입력
+		System.out.print("제거할 책 인덱스 번호 : ");
+		int index = sc.nextInt();
+		
+		
+		// 서비스 호출
+		BookDTO target = service.removeBook(index);
+		if(target == null) {
+			System.out.println("해당 인덱스에 책이 존재하지 않습니다.");
+			return;
+		} else {
+			System.out.printf("[%s] 책이 제거되었습니다.", target.getTitle());
+		}
+	}
 	
 	
+	/** 제목이 일치하는 책 정보 출력
+	 * 	단, 제목이 일치하는 책이 없다면 "검색 결과 없음" 출력
+	 * 
+	 */
+	private void selectTitle() {
+		System.out.println("\n ### 제목이 일치하는 책 조회하기 ### \n");
+		
+		// 조회할 책제목 입력
+		System.out.print("조회할 책 제목 입력 : ");
+		String title = sc.nextLine();
+		
+		// 서비스 호출
+		BookDTO target = service.selectTitle(title);
+		
+		if(target == null) {
+			System.out.println("검색 결과 없음.");
+		} else {
+			System.out.println("---------------------------------");
+			System.out.println("제목 : " + target.getTitle());
+			System.out.println("저자 : " + target.getAuthor());
+			System.out.println("가격 : " + target.getPrice());
+			System.out.println("출판사 : " + target.getPublisher());
+		}
+	}
 	
 	
+	/** 제목이 일치하는 책 제거하기
+	 *  단, 제목이 일치하는 책이 없다면 "검색 결과 없음" 출력
+	 */
+	private void removeBookTitle() {
+		System.out.println("\n ### 제목이 일치하는 책 조회하기 ### \n");
+		
+		// 조회할 책제목 입력
+		System.out.print("제거할 책 제목 입력 : ");
+		String title = sc.nextLine();
+		
+		// 서비스 호출
+		boolean result = service.removeBookTitle(title);
+		
+		if(result) { // true인 경우 삭제
+			System.out.printf("[%s]책이 제거되었습니다.", title);
+		} else {
+			System.out.println("검색 결과 없음.");
+			return;
+		}
+	}
 	
 	
+	/** 출판사가 일치하는 책 모두 조회하기
+	 *  단, 출판사가 일치하는 책이 없다면 "검색 결과 없음" 출력
+	 */
+	private void selectPublisherAll() {
+		System.out.println("\n ### 출판사가 일치하는 책 조회하기 ### \n");
+
+		// 조회할 출판사 입력
+		System.out.print("출판사 입력 : ");
+		String publisher = sc.nextLine();
+		
+		// 서비스 호출 (결과 개수 : 0 ~ n)
+		List<BookDTO> list = service.selectPublisherAll(publisher);
+		
+		// 조회 결과가 없을 경우
+		//if(list.size()==0) {
+		if(list.isEmpty()) { // list에 저장된 객체가 없을 경우
+			System.out.println("검색 결과 없음.");
+			return;
+		}
+		
+		for(BookDTO book : list) { // 있으면 다 출력
+			System.out.println(book);
+		}
+	}
 	
 	
+	/** 저자가 일치하는 책 모두 조회하기
+	 *  단, 저자가 일치하는 책이 없다면 "검색 결과 없음" 출력
+	 */
+	private void selectAuthorAll() {
+		System.out.println("\n ### 저자가 일치하는 책 조회하기 ### \n");
+		
+		// 저자 입력
+		System.out.print("저자 입력 : ");
+		String author = sc.nextLine();
+		
+		// 서비스 호출
+		List<BookDTO> list = service.selectAuthorAll(author);
+		
+		if(list.size()==0) {
+			System.out.println("검색 결과 없음.");
+			return;
+		}
+		
+		for(BookDTO book : list) { // 있으면 다 출력
+			System.out.println(book);
+		}
+	}
 	
 	
-	
-	
-	
+	/** 검색어가 제목,저자에 포함된 모든 책 조회하기
+	 *  단, 일치하는 책이 없다면 "검색 결과 없음" 출력
+	 */
+	private void searchBook() {
+		System.out.println("\n ### 제목,저자가 일치하는 책 조회하기 ### \n");
+		
+		// 검색어 입력
+		System.out.print("검색어 입력 : ");
+		String search = sc.nextLine();
+		
+		// 서비스 호출
+		List<BookDTO> searchList = service.searchBook(search);
+		
+		if(searchList.size()==0) {
+			System.out.println("검색 결과 없음.");
+			return;
+		}
+		
+		for(BookDTO book : searchList) { // 있으면 다 출력
+			System.out.println(book);
+		}
+	}
 }
