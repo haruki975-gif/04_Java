7bf5a24997d26dd3fb4e4db4fd097403168739bc
haruki975-gif <haruki975@gmail.com>
2025-02-12 09:38:33 +0900

    20250212_Read

---
+++ 12_io/src/section04/run/CharRun.java
@@ -0,0 +1,9 @@
+package section04.run;
+
+import section04.service.CharService;
+
+public class CharRun {
+	public static void main(String[] args) {
+		CharService service = new CharService(); 
+	}
+}
